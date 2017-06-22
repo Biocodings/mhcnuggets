743294a75067c9883e8ccf68d063be7f08364763
Rohit Bhattacharya <rohit.bhattachar@gmail.com>
2017-06-22 07:39:47 -0400

    writing readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# mhcnuggets
-MHCnuggets 1.0
+# MHCnuggets 1.0
+
+This repository contains scripts for training and making
+predictions using the MHCnuggets models! It also
+contains the pre-trained models for the Kim 2014 benchmark
+as seen in the MHCnuggets paper (link). Production models
+trained on the latest IEDB data are on the way.
+
+### Currently available models ###
+* MHCnuggets-LSTM
+* MHCnuggets-GRU
+* MHCnuggets-FC
+* MHCnuggets-Chunky-CNN
+* MHCnuggets-Spanny-CNN
+
+
+```bash
+python
+```
+
+First Header  | Second Header
+------------- | -------------
+Content Cell  | Content Cell
+Content Cell  | Content Cell
+
--- scripts/predict.py
+++ scripts/predict.py
@@ -1,5 +1,6 @@
 '''
-Training a GRU model
+Predict IC50s for a batch of peptides
+using a trained model
 
 Rohit Bhattacharya
 rohit.bhattachar@gmail.com
@@ -12,28 +13,8 @@ import numpy as np
 import os
 from keras_models import get_predictions
 import keras_models
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import f1_score
-from scipy.stats import kendalltau
 from keras.optimizers import Adam, SGD
 import argparse
-
-
-# various paths/constants probably need to be moved
-# somewhere more useful
-DATA_DIR = 'data/kim2014'
-WEIGHTS_DIR = 'saves/gru_finetune_weights/'
-TRAIN_PATH = os.path.join(DATA_DIR, 'train.tsv')
-TEST_PATH = os.path.join(DATA_DIR, 'test.csv')
-MAX_LEN = 11
-NUM_AAS = 21  # including pad X
-# NUM_EPOCH = 200
-NUM_EPOCH = 25
-NUM_HIDDEN = 64
-BATCH_SIZE = 32
-LR = 0.001
-BETA = 0.9
-np.random.seed(1437)  # for reproducibility
 
 
 def train(mhc, pepfile):
@@ -69,12 +50,12 @@ def parse_args():
     Parse user arguments
     '''
 
-    info = 'Test an RNN on an MHC from kim 2014 data'
+    info = 'Predict IC50 for a batch of peptides using a trained model'
     parser = argparse.ArgumentParser(description=info)
 
-    parser.add_argument('-m', '--mhc',
+    parser.add_argument('-m', '--model_path',
                         type=str, required=True,
-                        help='MHC molecule to train on')
+                        help='Path to the trained model')
 
     parser.add_argument('-p', '--peptides',
                         type=str, required=True,
